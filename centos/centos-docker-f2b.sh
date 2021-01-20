5786099e9da73bb696cfd20190ea9708f9c3285f
Santaro255 <45904701+Santaro255@users.noreply.github.com>
2021-01-20 13:05:51 +0300

    update centos scripts

--- centos/centos-docker-f2b.sh
+++ centos/centos-docker-f2b.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # Update CentOS 
-echo "Updating CentOS"
+echo -e "\nUpdating CentOS"
 yum update -y && yum upgrade -y
 
 # Installing packages
-echo "Installing packages"
+echo -e "\nInstalling packages"
 yum install epel-release vim mc yum-utils -y
 
 # Installing Fail2Ban
-echo "Installing Fail2Ban"
+echo -e "\nInstalling Fail2Ban"
 yum install fail2ban -y
 
 # Create fail2ban.local
-echo "Create fail2ban.local"
+echo -e "\nCreate fail2ban.local"
 cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
 
 # Create jail.local
-echo "Create jail.local"
+echo -e "\nCreate jail.local"
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
 # Create sshd jail
-echo "Create sshd jail"
+echo -e "\nCreate sshd jail"
 cat > /etc/fail2ban/jail.d/sshd.conf <<EOL
 [sshd]
 enabled = true
@@ -32,17 +32,29 @@ bantime = 86400
 EOL
 
 # Enable and start Fail2Ban
-echo "Enable and start Fail2Ban"
+echo -e "\nEnable and start Fail2Ban"
 systemctl enable fail2ban && systemctl start fail2ban
 
 # Adding docker repo
-echo "Adding docker repo"
+echo -e "\nAdding docker repo"
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 # Installing docker
-echo "Installing docker"
+echo -e "\nInstalling docker"
 yum install docker-ce docker-ce-cli containerd.io -y
 
 # Enable and start docker
-echo "Enable and start docker"
+echo -e "\nEnable and start docker"
 systemctl enable docker && systemctl start docker
+
+# Download docker compose
+echo -e "\nDownload docker compose"
+wget "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)"
+
+# Move docker compose file
+echo -e "\nMove docker compose file"
+mv ./docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+
+# Exec docker compose file
+echo -e "\nExec docker compose file"
+chmod +x /usr/local/bin/docker-compose
